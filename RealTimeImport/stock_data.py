7ce8e0aa5d06bda496b7cf24485d29586348cc91
Amar Pawar <pawaramar.pawar@gmail.com>
2021-08-12 18:49:00 +0530

    [Amar Pawar] Add:Function to import real time stock data in hbase table from given api

--- RealTimeImport/stock_data.py
+++ RealTimeImport/stock_data.py
@@ -10,6 +10,8 @@
 import happybase as hb
 from logging_handler import logger
 import csv
+import requests
+import os
 from dotenv import load_dotenv
 load_dotenv('.env')
 
@@ -38,5 +40,34 @@ def create_table():
         logger.info(f"Errorr!!{e}")
         connection.close()
 
+def import_into_hbase():
+    """
+    Description:
+        This function will import data from stock API in HBase table the we created.
+    """
+    try:
+        connection=create_hbase_connection()
+        table = connection.table('stock_data')
+        my_data = os.getenv("KEY")
+        CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=1min&slice=year1month1&apikey={}'.format(my_data)
+
+        with requests.Session() as s:
+            download = s.get(CSV_URL)
+            decoded_content = download.content.decode('utf-8')
+            cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+            next(cr)
+            my_list = list(cr)
+            for row in my_list:
+                table.put(row[0],
+                {'cf1:open': row[1],
+                'cf2:high':row[2],
+                'cf3:low': row[3],
+                'cf4:close': row[4],
+                'cf5:volume': row[5]})
+    except Exception as e:
+        logger.info(f"Error!!{e}")
+        connection.close()
+
 create_hbase_connection()
 create_table()
+import_into_hbase()
