6f82783f251294fde971fb5d7319c00773be73cb
Amar Pawar <pawaramar.pawar@gmail.com>
2021-08-12 18:45:21 +0530

    [Amar Pawar] Add:FUnction to create table in hbase

--- RealTimeImport/stock_data.py
+++ RealTimeImport/stock_data.py
@@ -25,4 +25,18 @@ def create_hbase_connection():
     except Exception as e:
         logger.info(e)
 
+def create_table():
+    """
+    Description:
+        This function will create table in hbase with given name and column family.
+    """
+    try:
+        connection = create_hbase_connection()
+        connection.create_table('stock_data',{'cf1':dict(max_versions=1),'cf2':dict(max_versions=1),'cf3':dict(max_versions=1),'cf4':dict(max_versions=1),'cf4':dict(max_versions=1),'cf5':dict(max_versions=1)})
+        logger.info("Table created")
+    except Exception as e:
+        logger.info(f"Errorr!!{e}")
+        connection.close()
+
 create_hbase_connection()
+create_table()
